5d2eb84cc9a9b8312abec12d6bb5c8ecf960960e
ginkfin <nguyenquochungy@gmail.com>
2026-07-05 16:40:02 +0700

    Create occupation.sql

---
+++ occupation.sql
@@ -0,0 +1,21 @@
+select [Doctor], [Professor], [Singer], [Actor] FROM(
+    select 
+    Name,
+    Occupation,
+row_number() over (partition by Occupation order by Name asc) as sthidfkk
+from OCCUPATIONS
+) A
+PIVOT (
+    
+    MAX(Name)
+    for Occupation in([Doctor], [Professor], [Singer], [Actor])
+) as B
+  
+--OCCUPATION(MEDIUM)-HACKERRANK--
+-STEP 1: USE 'row_number() over (partition by Occupation order by Name asc) as sthidfkk' TO LABEL EACH NAME IN ORDER WHICH HAS SAME OCCUPATION.
+ -EXAMPLE: A DOCTER 1
+           B DOCTER 2 
+-STEP 2: USE PIVOT TO CHANGE ROWS TO COLUMNS( HOWEVER, WHEN WE USE PIVOT, WE HAVE TO USE AGGREGATE FUNCTION IN IT. THEREFORE, I USE MAX FOR NAME JUST TO PASS ITS RULE)
+ -EXAMPLE: DOCTER  PROFESSOR
+              A      C
+              B      D
